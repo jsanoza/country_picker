f546a103b661668d340f8dc4ce46c60ccd6f5fe9
jsanoza <sanozajohn@gmail.com>
2022-07-06 23:43:10 +0800

    Initial Commit

---
+++ lib/src/country_list_bottom_sheet.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import 'country.dart';
+import 'country_list_theme_data.dart';
+import 'country_list_view.dart';
+
+void showCountryListBottomSheet({
+  required BuildContext context,
+  required ValueChanged<Country> onSelect,
+  VoidCallback? onClosed,
+  List<String>? favorite,
+  List<String>? exclude,
+  List<String>? countryFilter,
+  bool showPhoneCode = false,
+  CountryListThemeData? countryListTheme,
+  bool searchAutofocus = false,
+  bool showWorldWide = false,
+  required Widget headline,
+  required Widget icon,
+}) {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    backgroundColor: Colors.transparent,
+    builder: (_) => _builder(
+      context,
+      onSelect,
+      favorite,
+      exclude,
+      countryFilter,
+      showPhoneCode,
+      countryListTheme,
+      searchAutofocus,
+      showWorldWide,
+      headline,
+      icon,
+    ),
+  ).whenComplete(() {
+    if (onClosed != null) onClosed();
+  });
+}
+
+Widget _builder(
+  BuildContext context,
+  ValueChanged<Country> onSelect,
+  List<String>? favorite,
+  List<String>? exclude,
+  List<String>? countryFilter,
+  bool showPhoneCode,
+  CountryListThemeData? countryListTheme,
+  bool searchAutofocus,
+  bool showWorldWide,
+  Widget headline,
+  Widget icon,
+) {
+  final device = MediaQuery.of(context).size.height;
+  final statusBarHeight = MediaQuery.of(context).padding.top;
+  final height = countryListTheme?.bottomSheetHeight ?? device - (statusBarHeight + (kToolbarHeight / 1.5));
+
+  Color? _backgroundColor = countryListTheme?.backgroundColor ?? Theme.of(context).bottomSheetTheme.backgroundColor;
+  if (_backgroundColor == null) {
+    if (Theme.of(context).brightness == Brightness.light) {
+      _backgroundColor = Colors.white;
+    } else {
+      _backgroundColor = Colors.black;
+    }
+  }
+
+  final BorderRadius _borderRadius = countryListTheme?.borderRadius ??
+      const BorderRadius.only(
+        topLeft: Radius.circular(40.0),
+        topRight: Radius.circular(40.0),
+      );
+
+  return Container(
+    height: height,
+    decoration: BoxDecoration(
+      color: _backgroundColor,
+      borderRadius: _borderRadius,
+    ),
+    child: CountryListView(
+      headline: headline,
+      icon: icon,
+      onSelect: onSelect,
+      exclude: exclude,
+      favorite: favorite,
+      countryFilter: countryFilter,
+      showPhoneCode: showPhoneCode,
+      countryListTheme: countryListTheme,
+      searchAutofocus: searchAutofocus,
+      showWorldWide: showWorldWide,
+    ),
+  );
+}
